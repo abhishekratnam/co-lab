7a305b7dd4978c1de2a17ea4afa7351817fe6614
abhishek <anshu98@outlook.com>
2020-05-02 16:49:00 +0530

    different components

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate,logout
-from account.forms import RegistrationForm,AccountAuthenticationForm,AccountUpdateForm
+from account.forms import RegistrationForm #AccountAuthenticationForm,AccountUpdateForm
 from django import forms
 
 def registration_view(request):
@@ -13,7 +13,7 @@ def registration_view(request):
             raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email,password = raw_password)
             login(request,account)
-            return redirect('home/home.html')
+            return redirect('/home')
         else:
             context['registeration_form'] = form
     else:#get request
@@ -23,42 +23,42 @@ def registration_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('home/home.html')
-
-# Create your views here.
-def login_view(request):
-    context = {}
-    user = request.user
-    if user.is_authenticated:
-        return redirect('user_profile/profile.html')
-    if request.POST:
-        form = AccountAuthenticationForm(request.POST)
-        if form.is_valid():
-            password = request.POST['password']
-            email = request.POST['email']
-            user = authenticate(email=email, password=password)
-
-            if user:
-                login(request,user)
-                return redirect('user_profile/profile.html')
-    else:
-        form = AccountAuthenticationForm()
-    context['login_form'] = form
-    return render(request, 'account/login.html',context)
-
-def account_view(request):
-    if not request.user.is_authenticated:
-        return redirect("login")
-    context = {}
-    if request.POST:
-        form = AccountUpdateForm(request.POST,instance=request.user)
-        if form.is_valid():
-            form.save()
-    else:
-        form = AccountUpdateForm(initial= {
-            "email":request.user.email,
-            "username":request.user.username,
-            }
-        )
-    context['account_form'] = form
-    return render(request, 'account/account.html', context)
+    return redirect('home/base.html')
+#
+# # Create your views here.
+# def login_view(request):
+#     context = {}
+#     user = request.user
+#     if user.is_authenticated:
+#         return redirect('user_profile/profile.html')
+#     if request.POST:
+#         form = AccountAuthenticationForm(request.POST)
+#         if form.is_valid():
+#             password = request.POST['password']
+#             email = request.POST['email']
+#             user = authenticate(email=email, password=password)
+#
+#             if user:
+#                 login(request,user)
+#                 return redirect('user_profile/profile.html')
+#     else:
+#         form = AccountAuthenticationForm()
+#     context['login_form'] = form
+#     return render(request, 'account/login.html',context)
+#
+# def account_view(request):
+#     if not request.user.is_authenticated:
+#         return redirect("login")
+#     context = {}
+#     if request.POST:
+#         form = AccountUpdateForm(request.POST,instance=request.user)
+#         if form.is_valid():
+#             form.save()
+#     else:
+#         form = AccountUpdateForm(initial= {
+#             "email":request.user.email,
+#             "username":request.user.username,
+#             }
+#         )
+#     context['account_form'] = form
+#     return render(request, 'account/account.html', context)
--- home/views.py
+++ home/views.py
@@ -6,8 +6,4 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
     return render(request, 'home/home.html')
-=======
-    return render(request, 'home/base.html')
->>>>>>> 5f9c3a49477068462dfb9a81cdfc5447c4b0603b
